545bfbde0b60cc0b6aeb3fe14bc73e3f472f7bb9
takeshi8989 <htakeshi0614@gmail.com>
2024-10-28 08:52:47 -0700

    edit file name

--- algorithms/multi_armed_bandit/main.py
+++ algorithms/multi_armed_bandit/main.py
@@ -1,35 +1,4 @@
-import numpy as np
-
-
-class EpsilonGreedyBandit:
-    def __init__(self, k, epsilon):
-        self.k = k
-        self.epsilon = epsilon
-        self.Q = np.zeros(k)
-        self.N = np.zeros(k)
-
-    def bandit(self, action):
-        # Reward function
-        # Action number A mod 3 determines the mean of the reward distribution
-        action_group = action % 3 
-        return np.random.normal(loc=action_group, scale=1)
-
-    def choose_action(self):
-        if np.random.rand() < self.epsilon:
-            return np.random.choice(self.k)
-        else:
-            return np.argmax(self.Q)
-
-    def update(self, action, reward):
-        self.N[action] += 1
-        self.Q[action] += (1 / self.N[action]) * (reward - self.Q[action])
-
-    def run(self, num_iterations):
-        for _ in range(num_iterations):
-            action = self.choose_action()
-            reward = self.bandit(action)
-            self.update(action, reward)
-
+from epsilon_greedy import EpsilonGreedyBandit
 
 if __name__ == "__main__":
     k = 10
