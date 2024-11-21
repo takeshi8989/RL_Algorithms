a83dcde5e3f82b10c4d5fe84674d58fe73767de5
takeshi8989 <htakeshi0614@gmail.com>
2024-11-20 22:19:05 -0800

    update dqn

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 import random
 from collections import deque
 
@@ -22,11 +23,13 @@ class DQN(nn.Module):
 
 class DQNAgent:
     def __init__(self, state_dim, action_dim, hidden_dim=128, gamma=0.99,
-                 lr=1e-3, batch_size=64, memory_size=10000):
+                 lr=1e-3, batch_size=64, memory_size=10000, epsilon=1.0,
+                 epsilon_decay=0.995, epsilon_min=0.01, target_update_freq=10):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.gamma = gamma
         self.batch_size = batch_size
+        self.target_update_freq = target_update_freq
 
         # Replay memory
         self.memory = deque(maxlen=memory_size)
@@ -38,9 +41,9 @@ class DQNAgent:
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=lr)
 
         # Exploration parameters
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.995
-        self.epsilon_min = 0.01
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
 
     def select_action(self, state):
         if random.random() < self.epsilon:
@@ -54,7 +57,7 @@ class DQNAgent:
     def store_transition(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def train(self):
+    def train_step(self):
         if len(self.memory) < self.batch_size:
             return
 
@@ -89,3 +92,34 @@ class DQNAgent:
 
     def decay_epsilon(self):
         self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+
+    def train(self, env, num_episodes):
+        for episode in range(num_episodes):
+            state, _ = env.reset()
+            state = np.array(state, dtype=np.float32)
+            done = False
+            total_reward = 0
+
+            while not done:
+                action = self.select_action(state)
+                next_state, reward, terminated, truncated, _ = env.step(action)
+                next_state = np.array(next_state, dtype=np.float32)
+                done = terminated or truncated
+
+                # Store the transition
+                self.store_transition(state, action, reward, next_state, done)
+
+                # Perform a training step
+                self.train_step()
+
+                state = next_state
+                total_reward += reward
+
+            # Update the target network periodically
+            if episode % self.target_update_freq == 0:
+                self.update_target_network()
+
+            # Decay epsilon after each episode
+            self.decay_epsilon()
+
+            print(f'Episode: {episode + 1}, Total Reward: {total_reward}')
