7379f9d042d78963a8fa8ed7681dd635161b572d
takeshi8989 <htakeshi0614@gmail.com>
2024-11-26 13:34:58 -0800

    change num episodes

--- a2c/main.py
+++ a2c/main.py
@@ -6,4 +6,4 @@ state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
 agent = A2C(state_dim, action_dim, learning_rate=0.001, gamma=0.99)
 
-agent.train(env, num_episodes=2000)
+agent.train(env, num_episodes=1000)
